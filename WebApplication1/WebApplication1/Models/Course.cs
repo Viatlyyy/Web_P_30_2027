c23fdd53e230302da51328b1782c557bcc8b56bf
Виталий <krilow.vitaly007@gmail.com>
2026-02-15 17:54:03 +0300

    Настроил подключение к базе данных

--- WebApplication1/WebApplication1/Models/Course.cs
+++ WebApplication1/WebApplication1/Models/Course.cs
@@ -2,7 +2,6 @@
 {
     public class Course : EFModel
     {
-        public int Id { get; set; }
         public string Title { get; set; }
     }
 }
--- WebApplication1/WebApplication1/Models/Instructor.cs
+++ WebApplication1/WebApplication1/Models/Instructor.cs
@@ -2,7 +2,6 @@
 {
     public class Instructor : EFModel
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Department { get; set; }
