883e566919a78b36ac7e9adfc43dc6b2156e69a2
Viatlyyy <krilow.vitaly007@gmail.com>
2026-03-23 08:53:41 +0300

    Реализовал страницу преподавателей с возможностью добавления, удаления, редактирования

---
+++ WebApplication1/WebApplication1/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Instructors
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Instructor> Instructors { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Instructors = await _context.Instructors.ToListAsync();
+        }
+    }
+}
