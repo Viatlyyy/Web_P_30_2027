d1d34812749fbfff1105ffa89f74e6f9e8ccecd2
Viatlyyy <krilow.vitaly007@gmail.com>
2026-03-23 08:31:27 +0300

    Реализовал страницу студента с возможностями добавления, редактирования и удаления студента

---
+++ WebApplication1/WebApplication1/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            
+            Student.Name = $"{Student.FirstName} {Student.LastName}";
+
+            _context.Students.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Students { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Students = await _context.Students.ToListAsync();
+        }
+    }
+}
